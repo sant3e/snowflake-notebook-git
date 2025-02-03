b66e22bda2f05929af44927e8d02738f43a3bf0f
Diaconescu Tiberiu <89382386+sant3e@users.noreply.github.com>
2025-02-03 11:10:44 +0200

    Update create_sp.sql

--- create_sp.sql
+++ create_sp.sql
@@ -3,5 +3,5 @@ CREATE OR REPLACE PROCEDURE filter_by_column_value(tableName VARCHAR, column_val
     LANGUAGE PYTHON
     RUNTIME_VERSION = '3.8'
     PACKAGES = ('snowflake-snowpark-python')
-    IMPORTS = ('@demo.public.my_github_repo/branches/master/python-script.filter.py')
+    IMPORTS = ('@demo.public.my_github_repo/branches/master/python-script/filter.py')
     HANDLER = 'filter.filter_by_column_value';
