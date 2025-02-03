058c3b92b98e746c4266a5cf741bb0349be5ad67
Diaconescu Tiberiu <89382386+sant3e@users.noreply.github.com>
2025-02-03 11:02:25 +0200

    Create create_sp.sql

---
+++ create_sp.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE PROCEDURE filter_by_column_value(tableName VARCHAR, column_value VARCHAR)
+    RETURN TABLE (id NUMBER, COLUMN1 NUMBER, COLUMN2 DATE, COLUMN3 TEXT)
+    LANGUAGE PYTHON
+    RUNTIME_VERSION = '3.8'
+    PACKAGES = ('snowflake-snowpark-python')
+    IMPORTS = ('@demo.public.my_github_repo/branches/master/python-script.filter.py')
+    HANDLE = 'filter.filter_by_column_value';
