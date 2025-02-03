7423e8f287ad8b8d9a5e40c11e7b391aea78cbaf
Diaconescu Tiberiu <89382386+sant3e@users.noreply.github.com>
2025-02-03 11:14:33 +0200

    Update filter.py

--- python-script/filter.py
+++ python-script/filter.py
@@ -1,6 +1,6 @@
 from snowflake.snowpark.functions import col
 
 # function that will be called by stored procedure's import parameter
-def filter_by_column_value(sessions, table_name, column_value):
+def filter_by_column_value(session, table_name, column_value):
   df = session.table(table_name)
   return df.filter(col("COLUMN3") == column_value)
