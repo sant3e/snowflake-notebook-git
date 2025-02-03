dbb5b65ff854f1e5032fac64f4a0481a95311087
Diaconescu Tiberiu <89382386+sant3e@users.noreply.github.com>
2025-02-03 11:23:12 +0200

    Update filter.py

--- python-script/filter.py
+++ python-script/filter.py
@@ -4,4 +4,4 @@ import json
 def filter_by_column_value(session, table_name, column_value):
     df = session.table(table_name)
     filtered_df = df.filter(col("COLUMN3") == column_value)
-    return json.loads(filtered_df.to_json())
+    return filtered_df.to_pandas().to_dict(orient='records')
